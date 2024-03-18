c3597a0e626c0884b0629292ce463f8477a91c9a
Hüseyin Kaya <kayahuseyin_45@outlook.com>
2024-03-19 00:39:25 +0300

    Swagger Dark Mode

--- Program.cs
+++ Program.cs
@@ -54,7 +54,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(x =>
+    {
+        x.InjectStylesheet("/swagger-ui/SwaggerDark.css");
+    });
 
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
